9b05678e5a65abc3a0aa7d7e306212ec962cf7de
André Junior <andre.souza64@fatec.sp.gov.br>
2021-05-08 11:11:07 -0300

    Components Input and Header add

---
+++ src/components/Form/Input.tsx
@@ -0,0 +1,32 @@
+import { FormControl, FormLabel, Input as FormInput, InputProps as ChackraInputProps } from "@chakra-ui/react";
+
+interface InputProps extends ChackraInputProps {
+  name: string;
+  label?: string;
+  type: string;
+}
+
+export function Input({ name, label, ...rest }: InputProps) {
+  return (
+    <FormControl>
+      {label && (
+        <FormLabel htmlFor={name}>
+          {label}
+        </FormLabel>
+      )}
+
+      <FormInput
+        name={name}
+        id={name}
+        focusBorderColor="pink.500"
+        bgColor="gray.900"
+        variant="filled"
+        _hover={{ 
+          bgColor: 'gray.900'
+        }}
+        size="lg"
+        {...rest}
+      />
+    </FormControl>
+  )
+}
